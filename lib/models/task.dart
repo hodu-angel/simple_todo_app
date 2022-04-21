221bc44c529e863e3deee7d0a2b770b189df37a3
seohyoju <kinnylovemy@gmail.com>
2022-04-21 15:48:32 +0900

    Task class 재정의

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,9 +1,36 @@
 class Task {
-  final String? title;
+  // final String? title;
+  // bool finished;
+  //
+  // Task({
+  //   this.title,
+  //   this.finished = false,
+  // });
+
+  final String _id;
+  final String _title;
   bool finished;
 
+  String get id => _id;
+
+  String get title => _title;
+
   Task({
-    this.title,
-    this.finished = false,
-  });
+    required String title,
+  })  : finished = false,
+        _id = DateTime.now().millisecondsSinceEpoch.toString(),
+        _title = title;
+
+  Task.fromJson(dynamic json)
+      : _id = json['id'] ?? DateTime.now().millisecondsSinceEpoch.toString(),
+        _title = json['title'] ?? '',
+        finished = json['finished'] ?? false;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = _id;
+    map['title'] = title;
+    map['finished'] = finished;
+    return map;
+  }
 }
