dc9df40efc458dc5791851812db1c0a5d215354d
seohyoju <kinnylovemy@gmail.com>
2022-04-25 00:15:56 +0900

    hive_helper crud add

--- lib/hive_helper.dart
+++ lib/hive_helper.dart
@@ -3,13 +3,14 @@ import 'package:simple_todo_app/models/task.dart';
 
 const String TASK_BOX = 'TASK_BOX';
 
-class HiveHelper{
+class HiveHelper {
   static final HiveHelper _singleton = HiveHelper._internal();
+
   factory HiveHelper(){
     return _singleton;
   }
-  HiveHelper._internal();
 
+  HiveHelper._internal();
 
 
   Box<Task>? tasksBox;
@@ -18,4 +19,22 @@ class HiveHelper{
   Future openBox() async {
     tasksBox = await Hive.openBox(TASK_BOX);
   }
+
+  Future create(Task newTask) async {
+    return tasksBox!.add(newTask);
+  }
+
+  Future<List<Task>> read() async {
+    return tasksBox!.values.toList();
+  }
+
+  Future update(int index, Task updatedTask) async {
+    tasksBox!.putAt(index, updatedTask);
+  }
+
+  Future delete(int index) async {
+    tasksBox!.deleteAt(index);
+  }
+
+
 }
